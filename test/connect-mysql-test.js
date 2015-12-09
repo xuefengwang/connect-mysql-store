743abaf4b3a4cd59afa5b675e1f48c3464ba6b5f
xuefengwang <xuefengw@gmail.com>
2015-12-09 14:38:14 -0500

    test store set

--- test/connect-mysql-test.js
+++ test/connect-mysql-test.js
@@ -1,10 +1,11 @@
 'use strict';
 
 var assert = require('assert');
+var mysql = require('mysql');
 var session = require('express-session');
 var MySQLStore = require('../lib/connect-mysql')(session);
 
-describe('Session', function() {
+describe('Session store create', function() {
     describe('#options', function () {
         it('should return store object with database connected', function (done) {
             var store = new MySQLStore({
@@ -25,4 +26,33 @@ describe('Session', function() {
             });
         });
     });
+});
+
+describe('Session store methods', function (done) {
+
+    var url = 'mysql://travis@localhost/connect_mysql_test';
+    var store = new MySQLStore({ url: url });
+    var conn = mysql.createConnection(url);
+    var sid = 'test-session-sid';
+
+    before(function (done) {
+        conn.query("CREATE TABLE connect_mysql_test.sessions " +
+            "(sid varchar(255) NOT NULL, session varchar(2048) NOT NULL DEFAULT '', PRIMARY KEY (sid)) " +
+            "ENGINE=InnoDB DEFAULT CHARSET=utf8", function (err) {
+            done(err);
+        });
+    });
+
+    describe('#set()', function () {
+        it('should create session record', function (done) {
+            store.set(sid, {test: 'a'}, function (err) {
+                assert(!err);
+                done();
+            });
+        });
+    });
+
+    after(function () {
+        conn.query("DROP TABLE connect_mysql_test.sessions");
+    })
 });
