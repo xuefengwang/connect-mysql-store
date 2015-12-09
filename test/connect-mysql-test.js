a1ad03550532df6a223c6c7039542f700d1b2b5b
xuefengwang <xuefengw@gmail.com>
2015-12-09 14:57:55 -0500

    test store get/destroy/clear

--- test/connect-mysql-test.js
+++ test/connect-mysql-test.js
@@ -4,6 +4,7 @@ var assert = require('assert');
 var mysql = require('mysql');
 var session = require('express-session');
 var MySQLStore = require('../lib/connect-mysql')(session);
+var _ = require('lodash');
 
 describe('Session store create', function() {
     describe('#options', function () {
@@ -34,10 +35,13 @@ describe('Session store methods', function (done) {
     var store = new MySQLStore({ url: url });
     var conn = mysql.createConnection(url);
     var sid = 'test-session-sid';
+    var sessionTestData = {test: 'a'};
 
     before(function (done) {
         conn.query("CREATE TABLE IF NOT EXISTS connect_mysql_test.sessions " +
-            "(sid varchar(255) NOT NULL, session varchar(2048) NOT NULL DEFAULT '', PRIMARY KEY (sid)) " +
+            "(sid varchar(255) NOT NULL, session varchar(2048) NOT NULL DEFAULT '', " +
+            "updated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, " +
+            "PRIMARY KEY (sid)) " +
             "ENGINE=InnoDB DEFAULT CHARSET=utf8", function (err) {
             done(err);
         });
@@ -45,14 +49,57 @@ describe('Session store methods', function (done) {
 
     describe('#set()', function () {
         it('should create session record', function (done) {
-            store.set(sid, {test: 'a'}, function (err) {
+            store.set(sid, sessionTestData, function (err) {
                 assert(!err);
                 done();
             });
         });
     });
 
+    describe('#get()', function () {
+        it('should get session record', function (done) {
+            store.get(sid, function (err, session) {
+                assert(!err);
+                assert(_.isEqual(session, sessionTestData));
+                done();
+            });
+        });
+    });
+
+    describe('#destory()', function () {
+        it('should remove specified session', function (done) {
+            store.destroy(sid, function (err) {
+                assert(!err);
+                store.get(sid, function (err, session) {
+                    assert(!err);
+                    assert.equal(session, null);
+                    done();
+                })
+            });
+        });
+    });
+
+    describe('#clear()', function () {
+        it('should remove all sessions', function (done) {
+            store.set(sid, sessionTestData, function (err) {
+                assert(!err);
+                store.set(sid + '1', sessionTestData, function (err) {
+                    assert(!err);
+                    store.clear(function (err) {
+                        assert(!err);
+                        conn.query('SELECT count(*) total FROM sessions', function (err, result) {
+                            assert(!err);
+                            assert.equal(result.length, 1);
+                            assert.equal(result[0].total, 0);
+                            done();
+                        });
+                    });
+                })
+            })
+        });
+    });
+
     after(function () {
-        conn.query("DROP TABLE connect_mysql_test.sessions");
+        //conn.query("DROP TABLE IF EXISTS connect_mysql_test.sessions");
     })
 });
