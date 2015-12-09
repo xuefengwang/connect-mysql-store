30d2d1495db75dde9477afb85b9ab50e24500051
xuefengwang <xuefengw@gmail.com>
2015-12-09 13:19:04 -0500

    setup travis

---
+++ test/connect-mysql-test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var assert = require('assert');
+var session = require('express-session');
+var MySQLStore = require('../lib/connect-mysql')(session);
+
+describe('Session', function() {
+    describe('#options', function () {
+        it('should return store object with database connected', function () {
+            var store = new MySQLStore({
+                url: 'mysql://travis@localhost/connect-mysql-test'
+            });
+            assert.equal(store instanceof MySQLStore, true);
+        });
+    });
+});
