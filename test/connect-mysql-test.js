8ae6228562a507e442e58993c9dae4dc9b127e2c
xuefengwang <xuefengw@gmail.com>
2015-12-09 14:06:55 -0500

    test db connection

--- test/connect-mysql-test.js
+++ test/connect-mysql-test.js
@@ -6,11 +6,23 @@ var MySQLStore = require('../lib/connect-mysql')(session);
 
 describe('Session', function() {
     describe('#options', function () {
-        it('should return store object with database connected', function () {
+        it('should return store object with database connected', function (done) {
             var store = new MySQLStore({
                 url: 'mysql://travis@localhost/connect_mysql_test'
             });
-            assert.equal(store instanceof MySQLStore, true);
+            store.on('connected', function () {
+                assert(store instanceof MySQLStore);
+                done();
+            });
+        });
+        it('should emit error with bad db url', function (done) {
+            var store = new MySQLStore({
+                url: 'mysql://travis@localhost/unknown'
+            });
+            store.on('error', function (err) {
+                assert(err !== null);
+                done();
+            });
         });
     });
 });
